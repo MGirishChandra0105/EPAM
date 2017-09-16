c0c97d3c14bf293bc953317d60730644c6665846
MGirishChandra0105 <32007703+MGirishChandra0105@users.noreply.github.com>
2017-09-16 00:29:23 -0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+
+
+
+
+
+    public Student[] getStudents()
+    {
+
+	    try{
+	   /* Scanner stud1 = new Scanner(System.in);
+         Stud<a> stud1=new ArrayList<a>
+         stud1.add(1);
+         stud1.add(2);
+         stud1.add(3);*/
+         this.students=students;
+
+
+
+ 		if(Students==null)
+ 		{
+ 			throw new IllegalArgumentException ("error");
+ 		}
+
+ 		}
+ 		catch(Exception e)
+ 		{
+ 		    System.out.println("TRY AGAIN");
+ 		}
+
+ 		// Add your implementation here
+		return students;
+	}
+
+	}
+
+}
